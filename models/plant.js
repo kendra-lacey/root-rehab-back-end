79ec9f271f04633ac779496009e5d833742d062c
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-24 16:09:50 -0500

    update models/plant.js

---
+++ models/plant.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Plant extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Plant.init({
+    name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    },
+    photo: DataTypes.STRING,
+    profileId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'Plant',
+  });
+  return Plant;
+};
