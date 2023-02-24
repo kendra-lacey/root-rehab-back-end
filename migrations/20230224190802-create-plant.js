dc549517da5e5dccefab807869532e45fc2418ae
Kendra Lacey <101334078+kendra-lacey@users.noreply.github.com>
2023-02-24 16:14:26 -0500

    update migrations file

--- migrations/20230224190802-create-plant.js
+++ migrations/20230224190802-create-plant.js
@@ -10,13 +10,20 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       photo: {
         type: Sequelize.STRING
       },
       profileId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Profiles',
+          key: 'id',
+        },
       },
       createdAt: {
         allowNull: false,
